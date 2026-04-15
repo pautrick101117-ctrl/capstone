bb05d61378546f61de5520251417ab3a8e21b5dc
patrick <pautrick101117@gmail.com>
2026-04-15 11:56:20 +0800

    add voting structure for client

---
+++ client/src/pages/client/VotingCenter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// this will show only when logged in ofcourse?
+const VotingCenter = () => {
+  return (
+    <div>VotingCenter</div>
+  )
+}
+
+export default VotingCenter
